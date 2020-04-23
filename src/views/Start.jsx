f9ba08f273b97fd08c097b3806a94b05b5e1e614
Filip Johansson <filip.johansson@protonmail.com>
2020-04-23 11:29:56 +0200

    Add routing and refactor boilerplate

---
+++ src/views/Start.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import View from "../components/View";
+
+const StartView = () => (
+  <div>
+    <View>
+      <h2>This should be the landing page</h2>
+      <p>
+        Cheese and wine macaroni cheese cheese slices. Danish fontina cheese
+        slices taleggio fromage frais fromage frais cauliflower cheese queso
+        blue castello. Bavarian bergkase feta red leicester stilton caerphilly
+        cream cheese pecorino fondue. Dolcelatte pecorino bocconcini emmental
+        red leicester.
+      </p>
+    </View>
+  </div>
+);
+
+export default StartView;
