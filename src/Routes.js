6b38f2373c29c46cf25a274e4c4ef4fa8829d035
Filip Johansson <filip.johansson@protonmail.com>
2020-04-25 13:31:25 +0200

    Working signup form

--- src/Routes.js
+++ src/Routes.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import StartView from "./views/Start";
-import SignUpView from "./views/AddUser";
+import SignUpView from "./views/Signup";
 
 const Routes = () => {
   return (
---
+++ src/gRPC/userClient.js
@@ -0,0 +1,57 @@
+const { AddUserRequest, User } = require("./service_pb");
+const { UsersRPCClient } = require("./service_grpc_web_pb");
+
+const envoyURI = "http://localhost:8080";
+
+const client = new UsersRPCClient(envoyURI);
+
+const createUser = (userSpec) => {
+  const {
+    name,
+    address: { address, city, country, postcode },
+    contact: { email },
+  } = userSpec;
+
+  const user = new User();
+  const userAddress = new User.Address();
+
+  user.setName(name);
+
+  // Address fields
+  userAddress.setAddress(address);
+  userAddress.setCity(city);
+  userAddress.setCountry(country);
+  userAddress.setPostcode(postcode);
+
+  user.setAddress(userAddress);
+
+  // Contact details
+  const userEmail = new User.ContactDetails();
+  userEmail.setPlatform("EMAIL");
+  userEmail.setIdentifier(email);
+
+  user.setContactDetailsList(userEmail);
+
+  return user;
+};
+
+const addUser = (userSpec) => {
+  const request = new AddUserRequest();
+
+  request.setUser(createUser(userSpec));
+
+  client.addUser(request, {}, (error, response) => {
+    if (error) {
+      console.error(error);
+      throw new Error("gRPC request failed");
+    }
+    const [userId] = response.array;
+    console.log(userId);
+  });
+};
+
+const userClient = {
+  addUser,
+};
+
+export default userClient;
